6f441058afc914cb6ca6967887747f65cc023cec
devcom33 <devcom33@users.noreply.github.com>
2025-12-03 15:03:22 +0100

    setup project

---
+++ src/main/java/org/heymouad/bookingmanagementsystem/BookingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.heymouad.bookingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/heymouad/bookingmanagementsystem/BookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heymouad.bookingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
