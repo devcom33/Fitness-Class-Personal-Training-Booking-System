73195b514d8d0d026f092604f51503beb8580b36
devcom33 <devcom33@users.noreply.github.com>
2025-12-04 20:57:14 +0100

    feat: adding OneToOne mapping to user

--- src/main/java/org/heymouad/bookingmanagementsystem/entities/Instructor.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/entities/Instructor.java
@@ -1,11 +1,11 @@
 package org.heymouad.bookingmanagementsystem.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.Instant;
 import java.util.UUID;
 
 
@@ -13,17 +13,29 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "instructors")
 public class Instructor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    /*
-    @OneToMany
-    @JoinColumn(name = "class_schedules_id")
-    private ClassSchedules classSchedules;*/
+
+    // optional = false - cannot have an instructor record without a corresponding user
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
 
     @Column(nullable = false)
     private String bio;
+
+    @Column(nullable = true)
+    private String specialization;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant modifiedAt;
 }
